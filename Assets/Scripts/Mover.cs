bc67204cf8b60f9bdbd8d4af7f2e8f394e1953f9
Clervens Volcy <clervens.volcy@gmail.com>
2017-09-16 18:20:42 -0400

    Finish step 1 of the tutorial

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed = 20;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		Transform transform = GetComponent<Transform> ();
+		rigidbody.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour {
 	public float tilt = 4;
 	public PlayerBoundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,7 +21,10 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 
 	void FixedUpdate () {
