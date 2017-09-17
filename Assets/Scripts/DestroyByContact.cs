6832c8e32344e0f60709029b9e589ce901bc92a1
Clervens Volcy <clervens.volcy@gmail.com>
2017-09-17 10:23:02 -0400

    Finish step 2 of the tutorial

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	private Transform transform;
+
+	// Use this for initialization
+	void Start () {
+		transform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+
+		Destroy (other.gameObject);
+		Destroy (gameObject);	
+	}
+}
